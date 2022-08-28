5b9cd4d01a9c8c992b1da76f83abaf28f1b9729e
Jason Miller <jasonmiller.dev@gmail.com>
2022-08-28 21:13:39 +1000

    MexicanWaveJS

---
+++ MexicanWaveJS/main.js
@@ -0,0 +1,13 @@
+function wave(str) {
+  // Code here
+  let waveArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (letter === " ") {
+      continue;
+    } else {
+      waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1));
+    }
+  }
+  return waveArr;
+}
